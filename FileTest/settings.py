28f7bcab9ae828de3a08082763f66274eceb2078
kwazart <artdd52669@gmail.com>
2022-03-15 14:30:14 +0300

    add new data

--- FileTest/settings.py
+++ FileTest/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$u_kb$$%%)qng31xhso2#e_^p372mu6cwd^2--1nq%_knvjcm@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
